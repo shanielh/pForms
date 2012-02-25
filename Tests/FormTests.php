000eae10281c3715abe7161698e9018611833067
Shani Elharrar <atnt123@bezeqint.net>
2012-02-25 12:52:11 +0200

    Added tests environment

---
+++ Tests/FormTests.php
@@ -0,0 +1,13 @@
+<?php
+
+use pUnit\Assert as Assert;
+
+class FormTests
+{
+    
+    public function One_Should_Equal_To_Zero()
+    {
+        Assert::AreEqual(1,0);    
+    }
+    
+}
---
+++ Tests/index.php
@@ -0,0 +1,33 @@
+<?php
+
+$pUnitDir = realpath("../Vendor/pUnit");
+set_include_path(get_include_path() . PATH_SEPARATOR . $pUnitDir . '/src/external/mockery/library/');
+
+require_once($pUnitDir . '/src/autoloader.php');
+require_once('Mockery/Loader.php');
+
+$loader = new \Mockery\Loader();
+$loader->register();
+
+\Mockery::getConfiguration()->allowMockingMethodsUnnecessarily(false);
+
+
+
+use pUnit\Assert as Assert;
+
+$pattern = '/.php$/i';
+$classExport = function($fileName) {
+    return substr($fileName,0 ,-4);
+
+};
+$provider = new pUnit\FolderTestProvider(__DIR__, $pattern, $classExport);
+
+$runner = new pUnit\TestRunner();
+$formatter = new pUnit\TestResultFormatters\CompositeTestResultFormatter(array(
+    new pUnit\TestResultFormatters\GrowlTestResultFormatter('127.0.0.1','password'),
+    new pUnit\TestResultFormatters\ConsoleTestResultFormatter(false)
+    ));
+$runner->SetOutput($formatter);
+$runner->SetTest($provider);
+
+$runner->Run();
