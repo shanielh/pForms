162895da430e08bef21746024c3148fbfa661b5c
Shani Elharrar <atnt123@bezeqint.net>
2012-02-25 13:05:04 +0200

    First u.t

--- Tests/FormTests.php
+++ Tests/FormTests.php
@@ -8,6 +8,9 @@ class FormTests
     public function Add_Should_Return_FormElement()
     {
         $form = new pForms\Form(null, null);
+        $retVal = $form->Add('Hey');
+        
+        Assert::IsInstanceOf('pForms\FormElement', $retVal);
     }
     
 }
